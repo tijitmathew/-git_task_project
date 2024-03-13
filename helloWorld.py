a46c2dab6ee8f1e2dd9b93ed47734436ecb24594
tijitmathew <tijitmathew99@email.com>
2024-03-13 23:53:20 +0000

    Comments added

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
-print("Hello World!")
+print("Hello World!") # Printing Hello World
 
-print("Git is awesome!")
+print("Git is awesome!") # Printing as per task 1
 
-a = input("Enter any data: ")
+a = input("Enter any data: ") # Getting input from User
 
-print(f"You inputted: {a} ")
+print(f"You inputted: {a} ") # Printing the input from user
