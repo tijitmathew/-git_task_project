69e8298591262235ba210e21e29739e8b073059e
tijitmathew <tijitmathew99@email.com>
2024-03-14 00:45:21 +0000

    Comments added to read code easily

--- PT3_age.py
+++ PT3_age.py
@@ -1,12 +1,15 @@
-name = input("Enter your name:")
+name = input("Enter your name:") # Requesting user to input name 
 
-age = int(input("Enter your age:"))
+age = int(input("Enter your age:")) # Requesting user to input age
 
-if age <= 10:
-    print(f"Long way to go dear {name}")
+if age <= 10: # Printing reponse using if elif for each age category.
+    print(f"Long way to go dear {name}") # Respose for age below or equal 10
+
 elif age <= 30:
-    print(f"Your are a family man Mr.{name}")
+    print(f"Your are a family man Mr.{name}") # Respose for age below or equal 30
+
 elif age <= 50:
-    print(f"Enjoy your peak time in your life Mr.{name}")
+    print(f"Enjoy your peak time in your life Mr.{name}") # Respose for age below or equal 50
+
 else:
-    print("Prepare for your retirement life")
+    print("Prepare for your retirement life") # Respose for age above 50
