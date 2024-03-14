4716e501d6026919b99ec7a7f9044f005054a6a4
tijitmathew <tijitmathew99@email.com>
2024-03-14 00:33:51 +0000

    Variables name changed to name & age.

--- PT3_age.py
+++ PT3_age.py
@@ -1,12 +1,12 @@
-a = input("Enter your name:")
+name = input("Enter your name:")
 
-b = int(input("Enter your age:"))
+age = int(input("Enter your age:"))
 
-if b <= 10:
-    print(f"Long way to go dear {a}")
-elif b <= 30:
-    print(f"Your are a family man Mr.{a}")
-elif b <= 50:
-    print(f"Enjoy your peak time in your life Mr.{a}")
+if age <= 10:
+    print(f"Long way to go dear {name}")
+elif age <= 30:
+    print(f"Your are a family man Mr.{name}")
+elif age <= 50:
+    print(f"Enjoy your peak time in your life Mr.{name}")
 else:
     print("Prepare for your retirement life")
